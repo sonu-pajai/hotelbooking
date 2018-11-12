ff1d8dbddc81e48270620feaaaadd913619a326d
Sonu pajai <sonupajai22@gmail.com>
2018-11-12 18:26:33 +0530

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require_relative 'boot'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 module Hotelbooking
   class Application < Rails::Application
